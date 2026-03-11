b01624a31a1df7b56f6773ad5d98f2545de2d8a0
9527 <95906363+2405008450@users.noreply.github.com>
2026-03-11 11:18:23 +0800

    增加子表单，优化前端

---
+++ frontend/src/api/subOrders.js
@@ -0,0 +1,47 @@
+import api from './index'
+
+function toCamelCase(str) {
+    return str.replace(/([-_][a-z])/g, (group) =>
+        group.toUpperCase().replace('-', '').replace('_', '')
+    )
+}
+
+function toSnakeCase(str) {
+    return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
+}
+
+function convertKeys(obj, converter) {
+    if (Array.isArray(obj)) {
+        return obj.map(v => convertKeys(v, converter))
+    } else if (obj !== null && obj.constructor === Object) {
+        return Object.keys(obj).reduce((result, key) => {
+            result[converter(key)] = convertKeys(obj[key], converter)
+            return result
+        }, {})
+    }
+    return obj
+}
+
+export const getSubOrders = (params) => {
+    return api.get('/sub-orders/', { params }).then(res => convertKeys(res, toCamelCase))
+}
+
+export const getSubOrdersByProject = (projectId) => {
+    return api.get(`/sub-orders/project/${projectId}`).then(res => convertKeys(res, toCamelCase))
+}
+
+export const getSubOrder = (id) => {
+    return api.get(`/sub-orders/${id}`).then(res => convertKeys(res, toCamelCase))
+}
+
+export const createSubOrder = (data) => {
+    return api.post('/sub-orders/', convertKeys(data, toSnakeCase)).then(res => convertKeys(res, toCamelCase))
+}
+
+export const updateSubOrder = (id, data) => {
+    return api.put(`/sub-orders/${id}`, convertKeys(data, toSnakeCase)).then(res => convertKeys(res, toCamelCase))
+}
+
+export const deleteSubOrder = (id) => {
+    return api.delete(`/sub-orders/${id}`)
+}
