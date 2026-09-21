a0a3dc61d7c28dca257c1e9433c30248f56ead5e
9527 <95906363+2405008450@users.noreply.github.com>
2026-09-21 16:09:36 +0800

    更新前端版本标识至 V1.6

--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -174,8 +174,8 @@ const { openPanel, syncFromStorage } = useUiZoom()
 
 const STORAGE_COLLAPSE_KEY = 'sidebar_collapse'
 const SYSTEM_MENU_INDEX = 'system-management'
-const appVersion = 'V1.5'
-const appUpdatedAt = '2026-09-15 14:34'
+const appVersion = 'V1.6'
+const appUpdatedAt = '2026-09-21 16:09'
 
 /** 侧边栏是否折叠 */
 const isCollapse = ref(false)
